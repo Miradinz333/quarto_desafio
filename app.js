3f5df2a9c3442c41de85cf02fbfa8b35dba42320
Deize <diselzap@gmail.com>
2025-04-29 17:47:30 -0300

    primeira versão

---
+++ app.js
@@ -0,0 +1,14 @@
+let listaGenerica = []
+
+let linguagensDeProgramacao = ['Java' , 'Ruby' , 'GoLang']
+linguagensDeProgramacao.push('javaScript', 'C' , 'C++' , 'Kotlin' , 'Python');
+console.log(linguagensDeProgramacao);
+
+let listaNomes = ['Lucas' , 'Menezes' , 'Souza'];
+console.log(listaNomes[0]);
+
+let listaNomesDois = ['Julia' , 'Rinaldi' , 'Soler'];
+console.log(listaNomesDois[1]);
+
+let listaNomesTres = ['Disel' , 'Teixeira' , 'Lima'];
+console.log(listaNomesTres[2]);
